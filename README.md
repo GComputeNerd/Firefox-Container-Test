6dc8c6a84ce1896da449b1bb29d2455aa7cda30b
GComputeNerd <GComputeNerd@users.noreply.github.com>
2020-07-20 22:48:18 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Firefox Container Fun Site
+
+This is a small website, made to play around with the container functions in firefox.
---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+let workButton = document.getElementById("Work");
+let personalButton = document.getElementById("Personal");
+let noneButton = document.getElementById("None");
+let myHeading = document.querySelector("h1");
+let myBody = document.querySelector("body");
+let myImg = document.querySelector("img");
+let myMode = localStorage.getItem('test-container-mode');
+
+function setContainerToWork() {
+		myHeading.textContent = "This is a Work Container!";
+		myBody.setAttribute("bgcolor", "#7070db");
+		myImg.setAttribute("src", "img/Suitcase.png");
+		localStorage.setItem("test-container-mode", "work");
+}
+
+function setContainerToPersonal() {
+		myHeading.textContent = "This is a Personal Container!";
+		myBody.setAttribute("bgcolor", "#b366ff");
+		myImg.setAttribute("src", "img/Fingerprint.png");
+		localStorage.setItem("test-container-mode", "personal")
+}
+
+function setContainerToNone() {
+		myHeading.textContent = "This is in No Container!";
+		myBody.setAttribute("bgcolor", "#ffb366");
+		myImg.setAttribute("src", "img/Firefox-Logo.png");
+		localStorage.setItem("test-container-mode", "none");
+}
+
+if(!myMode || myMode == "none") {
+		setContainerToNone();
+}
+else if (myMode == "work") {
+		setContainerToWork();
+}
+else if (myMode == "personal") {
+		setContainerToPersonal();
+}
+
+workButton.onclick = function() {
+		setContainerToWork();
+}
+
+personalButton.onclick = function() {
+		setContainerToPersonal();
+}
+
+noneButton.onclick = function() {
+		setContainerToNone();
+}
